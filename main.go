022e34e9a8172c2e54411eb1e7a90ff52eecfbb8
rishusingh022 <rishusingh022@gmail.com>
2023-10-15 16:27:27 +0530

    learn: slice declaration and iteration

--- main.go
+++ main.go
@@ -2,8 +2,21 @@ package main
 
 import "fmt"
 
+// var deckSize int
+
 func main() {
-	cards := newCard()
+	// var cards string = "Ace of Spades"
+	// cards := "Ace of Spades"
+	// cards = "Five of Diamonds"
+	// cards := newCard()
+
+	cards := []string{newCard(), "Ace of Diamonds"}
+	cards = append(cards, "Six of Spades")
+
+	for i, cards := range cards {
+		fmt.Println(i, cards)
+	}
+
 	fmt.Println(cards)
 }
 
