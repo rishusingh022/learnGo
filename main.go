20f5b7a4789a60a2385a13f3a013de4c672efb8c
rishusingh022 <rishusingh022@gmail.com>
2023-10-15 20:04:10 +0530

    learn: reciever funtions

--- main.go
+++ main.go
@@ -1,23 +1,11 @@
 package main
 
-import "fmt"
-
-// var deckSize int
-
 func main() {
-	// var cards string = "Ace of Spades"
-	// cards := "Ace of Spades"
-	// cards = "Five of Diamonds"
-	// cards := newCard()
 
-	cards := []string{newCard(), "Ace of Diamonds"}
+	cards := deck{newCard(), "Ace of Diamonds"}
 	cards = append(cards, "Six of Spades")
 
-	for i, cards := range cards {
-		fmt.Println(i, cards)
-	}
-
-	fmt.Println(cards)
+	cards.print()
 }
 
 func newCard() string {
