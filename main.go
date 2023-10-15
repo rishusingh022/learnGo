4a666f6109a0db112fe10eb59324d896c229d1fd
rishusingh022 <rishusingh022@gmail.com>
2023-10-15 16:12:37 +0530

    learn: function declaration

--- main.go
+++ main.go
@@ -2,12 +2,11 @@ package main
 
 import "fmt"
 
-var deckSize int
-
 func main() {
-	// var cards string = "Ace of Spades"
-	deckSize = 52
-	cards := "Ace of Spades"
-	cards = "Five of Diamonds"
+	cards := newCard()
 	fmt.Println(cards)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
