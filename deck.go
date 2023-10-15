765562c75257947259a1ad63a1c3ec9f723c9cef
rishusingh022 <rishusingh022@gmail.com>
2023-10-16 00:10:03 +0530

    learn: reading from file

--- deck.go
+++ deck.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 	"strings"
 )
 
@@ -39,4 +40,16 @@ func (d deck) toString() string {
 
 func (d deck) saveToFile(filename string) error {
 	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		// Option #1 - log the error and return a call to newDeck()
+		// Option #2 - log the error and entirely quit the program
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+	s := strings.Split(string(bs), ",")
+	return deck(s)
 }
--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 func main() {
 
-	cards := newDeck()
+	// cards := newDeck()
+	// cards.print()
+	// cards.saveToFile("my_cards")
+	cards := newDeckFromFile("my_cards")
 	cards.print()
-	cards.saveToFile("my_cards")
 }
