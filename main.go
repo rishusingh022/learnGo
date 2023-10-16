305a70cdb3c113cad3fe6cab207555e69f0787c4
rishusingh022 <rishusingh022@gmail.com>
2023-10-16 15:31:46 +0530

    add: assignment solution

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 
 	// cards := newDeck()
@@ -11,4 +13,15 @@ func main() {
 	cards := newDeck()
 	cards.shuffle()
 	cards.print()
+
+	// Assignment solution
+	arrayNumber := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+	for _, num := range arrayNumber {
+		if num%2 == 0 {
+			fmt.Printf("Number %v is even\n", num)
+		} else {
+			fmt.Printf("Number %v is odd\n", num)
+		}
+	}
 }
