56216d5492ae8e3cd634dc69391fcda0662f0f97
Swapnilswiggy06 <swapnil.singh_int@external.swiggy.in>
2023-10-17 19:21:59 +0530

    fix: tests

--- deck_test.go
+++ deck_test.go
@@ -12,11 +12,11 @@ func TestNewDeck(t *testing.T) {
 		t.Errorf("Expected deck length of 20, but got %v", len(d))
 	}
 
-	if d[0] != "Ace of Spades" {
+	if d[0] != "Spades of Ace" {
 		t.Errorf("Expected first card of Ace of Spades, but got %v", d[0])
 	}
 
-	if d[len(d)-1] != "Four of Clubs" {
+	if d[len(d)-1] != "Clubs of Four" {
 		t.Errorf("Expected last card of Four of Clubs, but got %v", d[len(d)-1])
 	}
 }
