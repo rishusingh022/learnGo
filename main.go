8448f0d873dfdbd6e713e2573786a41fcc40ee8b
Swapnilswiggy06 <swapnil.singh_int@external.swiggy.in>
2023-10-19 13:02:46 +0530

    learn: go routine and http call

--- main.go
+++ main.go
@@ -134,6 +134,32 @@ func main() {
 	}
 	io.Copy(os.Stdout, f)
 
+	// channel and routines
+
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+
+	// go keyword is used to create a new routine and run the function in the background and main routine will not wait for the function to complete and it will continue to execute the next line of code
+
+	for _, link := range links {
+		go checkLink(link)
+	}
+
+}
+
+func checkLink(link string) {
+	_, err := http.Get(link)
+
+	if err != nil {
+		fmt.Println(link, "might be down")
+		return
+	}
+	fmt.Println(link, "is up")
 }
 
 func printGreeting(b bot) {
